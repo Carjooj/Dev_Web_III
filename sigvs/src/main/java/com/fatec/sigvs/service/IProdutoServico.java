f936c17c40683347b6b305466afd7522aa49e221
Victor Cardoso Andrade <112758492+Carjooj@users.noreply.github.com>
2023-08-29 11:22:12 -0300

    Aula 29/08

---
+++ sigvs/src/main/java/com/fatec/sigvs/service/IProdutoServico.java
@@ -0,0 +1,15 @@
+package com.fatec.sigvs.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.fatec.sigvs.model.Produto;
+
+public interface IProdutoServico {
+	public List<Produto> consultaCatalogo();
+	public List<Produto> consultaPorDescricao();
+	public Optional <Produto> cadastrar(Produto produto);
+	public Optional <Produto> consultarPorId(String id);
+	public Optional <Produto> atualizar(Long id, Produto produto);
+	public void excluir(Long id);
+}
