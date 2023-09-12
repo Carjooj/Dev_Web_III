09a59386e25424c198137096befb44acdba1b15d
dti <dti@LAB204-29>
2023-09-12 11:17:22 -0300

    Função - Consulta catalogo com imagem

--- sigvs/src/main/java/com/fatec/sigvs/service/IProdutoServico.java
+++ sigvs/src/main/java/com/fatec/sigvs/service/IProdutoServico.java
@@ -3,10 +3,11 @@ package com.fatec.sigvs.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.fatec.sigvs.model.Catalogo;
 import com.fatec.sigvs.model.Produto;
 
 public interface IProdutoServico {
-	public List<Produto> consultaCatalogo();
+	public List<Catalogo> consultaCatalogo();
 	public List<Produto> consultaPorDescricao();
 	public Optional <Produto> cadastrar(Produto produto);
 	public Optional <Produto> consultarPorId(String id);
--- sigvs/src/main/java/com/fatec/sigvs/service/ProdutoServico.java
+++ sigvs/src/main/java/com/fatec/sigvs/service/ProdutoServico.java
@@ -1,22 +1,41 @@
 package com.fatec.sigvs.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fatec.sigvs.model.Catalogo;
+import com.fatec.sigvs.model.IImagemRepository;
 import com.fatec.sigvs.model.IProdutoRepository;
+import com.fatec.sigvs.model.Imagem;
 import com.fatec.sigvs.model.Produto;
 
 @Service
 public class ProdutoServico implements IProdutoServico {
 	@Autowired
-	IProdutoRepository repository;
+	IProdutoRepository repositoryP;
+	@Autowired
+	IImagemRepository repositoryI;
+
 	@Override
-	public List<Produto> consultaCatalogo() {
-		// TODO Auto-generated method stub
-		return repository.findAll();
+	public List<Catalogo> consultaCatalogo() {
+		Catalogo c = null;
+		List<Catalogo> lista = new ArrayList<>();
+		List<Produto> listaP = repositoryP.findAll();
+		List<Imagem> listaI = repositoryI.findAll();
+		for (Produto p : listaP) {
+			for (Imagem i : listaI) {
+				if (p.getId().equals(i.getId())) {
+					c = new Catalogo(p.getId(), p.getDescricao(), p.getCategoria(), p.getCusto(),
+							p.getQuantidadeNoEstoque(), i.getArquivo());
+					lista.add(c);
+				}
+			}
+		}
+		return lista;
 	}
 
 	@Override
@@ -46,7 +65,7 @@ public class ProdutoServico implements IProdutoServico {
 	@Override
 	public void excluir(Long id) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
